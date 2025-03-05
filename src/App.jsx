06b493f67f92fef9c932044fb219b15832ba1aa8
Dabarera <75031785+Zenith1997@users.noreply.github.com>
2025-03-05 03:55:04 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import "./App.css";
+import { useQuery
+  import axios from "axios";
+ } from "react-query";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+
+
+      </div>
+    </>
+  );
+}
+
+export default App;
